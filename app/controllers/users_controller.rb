fa6586fffde15b1ee8278376cf188f341f5304d4
n-s0120 <satoshicuni@gmail.com>
2021-05-18 10:28:25 +0000

    kadai3-0518-1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,37 @@
 class UsersController < ApplicationController
+
+  def new
+    @users = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.user_id = current_user.id
+    @user.save
+    redirect_to users_path
+  end
+
+  def index
+    @users = User.all
+  end
+
   def show
-   @user = User.find(params[:id])
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to user_path
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:name).permit(:name, :email)
   end
 end
